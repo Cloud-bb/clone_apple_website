bd54b852eb90ccd93621141226334eb9c0397258
Cloud-bb <taay_429@naver.com>
2026-03-27 18:11:04 +0900

    style: 화면 사이즈에 따라 섹션 사이즈 변경

---
+++ js/main.js
@@ -0,0 +1,88 @@
+(() => {
+
+    let yOffset = 0; //window.pageYoffset
+    let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
+    let currentScene = 0; //현재 활성화된 씬(scrollSection)
+    const sceneInfo = [
+
+        {
+            //0
+            type: 'sticky',
+            heightNum: 5, //브라우저 높이의 5배로 scrollHeight세팅
+            scrollHeight: 0,
+            objs: {
+                container: document.querySelector("#scroll-section-0")
+            },
+        },
+        {
+            //1
+            type: 'normal',
+            heightNum: 5, //브라우저 높이의 5배로 scrollHeight세팅
+            scrollHeight: 0,
+            objs: {
+                container: document.querySelector("#scroll-section-1")
+            },
+        },
+        {
+            //2
+            type: 'sticky',
+            heightNum: 5, //브라우저 높이의 5배로 scrollHeight세팅
+            scrollHeight: 0,
+            objs: {
+                container: document.querySelector("#scroll-section-2")
+            },
+        },
+        {
+            //3
+            type: 'sticky',
+            heightNum: 5, //브라우저 높이의 5배로 scrollHeight세팅
+            scrollHeight: 0,
+            objs: {
+                container: document.querySelector("#scroll-section-3")
+            },
+        },
+    ];
+
+    function setLayout() {
+        //각 스크롤 세션의 높이 세팅
+        for (let i = 0; i < sceneInfo.length; i++) {
+            sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+            sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+        }
+
+        //새로고침 시에도 
+        let totalScrollHeight = 0;
+        for(let i =0;i<sceneInfo.length;i++){
+            totalScrollHeight+=sceneInfo[i].scrollHeight;
+            if(totalScrollHeight >= yOffsetOffset){
+                currentScene=i;
+                break;
+            }
+            
+        }
+        document.body.setAttribute('id',`show-scene-${currentScene}`);
+    }
+
+    function scrollLoop(){
+        prevScrollHeight = 0;
+        for(i=0;i<currentScene;i++){
+            prevScrollHeight += sceneInfo[i].scrollHeight;
+        }
+
+        if(yOffset>prevScrollHeight+sceneInfo[currentScene].scrollHeight){
+            currentScene++;
+        }
+        if(yOffset < prevScrollHeight){
+            if(currentScene == 0) return; // 브라우저 바운스 효과로 인해 마이너스가 되는것을 방지
+            currentScene--;
+        }
+        document.body.setAttribute('id',`show-scene-${currentScene}`)
+    }
+    
+    window.addEventListener('scroll',()=>{
+        yOffset = window.pageYOffset;
+        scrollLoop();
+    })
+    window.addEventListener('resize',setLayout);
+    window.addEventListener('load',setLayout)
+})(); 
